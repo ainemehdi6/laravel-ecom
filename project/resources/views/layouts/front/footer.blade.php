78e91925f4421feea3985e5cf451436aa17494a1
EL AINE El Mehdi <ainemehdi6@gmail.com>
2024-06-20 15:07:21 +0200

    FIx : footer

--- project/resources/views/layouts/front/footer.blade.php
+++ project/resources/views/layouts/front/footer.blade.php
@@ -5,19 +5,7 @@
 
                 <ul class="footer-menu">
                     <li> <a href="{{ route('accounts', ['tab' => 'profile']) }}">Your account</a>  </li>
-                    <li> <a href="">Contact us</a>  </li>
-                    <li> <a href="">Terms of service</a>  </li>
                 </ul>
-
-                <ul class="footer-social">
-                    <li> <a href=""> <i class="fa fa-facebook" aria-hidden="true"></i>  </a> </li>
-                    <li> <a href=""> <i class="fa fa-twitter" aria-hidden="true"></i>   </a> </li>
-                    <li> <a href=""> <i class="fa fa-instagram" aria-hidden="true"></i>  </a> </li>
-                    <li> <a href=""> <i class="fa fa-pinterest-p" aria-hidden="true"></i>  </a> </li>
-                </ul>
-
-                <p>&copy; <a href="{{ config('app.url') }}">{{ config('app.name') }}</a> | All Rights Reserved</p>
-
             </div>
         </div>
     </div>
