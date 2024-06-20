3b3757766aaecc3d6272740d7a8577fbafb8d0ab
EL AINE El Mehdi <ainemehdi6@gmail.com>
2024-06-20 11:50:41 +0200

    Fix : Fix admin

--- project/config/bank-transfer.php
+++ project/config/bank-transfer.php
@@ -2,7 +2,7 @@
 
 return [
     'name' => 'Bank Transfer',
-    'description' => 'Online / Offline Bank fund transfer',
+    'description' => 'Offline Bank fund transfer',
     'bank_name' => env('BANK_TRANSFER_NAME', 'Bank of Mars'),
     'account_type' => env('BANK_TRANSFER_ACCOUNT_TYPE', 'Savings Account (SA)'),
     'account_name' => env('BANK_TRANSFER_ACCOUNT_NAME', 'John Doe'),
